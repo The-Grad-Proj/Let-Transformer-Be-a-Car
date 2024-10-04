8f6823a4ba5d2b094d56ba71dc5883fe4525d5cb
Ahmed Baheyeldin <142495693+Baheeyy@users.noreply.github.com>
2024-10-04 10:18:30 +0300

    Update EvalDataset.py to add SEA-RAFT

--- DataLoading/EvalDataset.py
+++ DataLoading/EvalDataset.py
@@ -1,34 +1,20 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov  7 12:57:21 2021
-
-@author: chingis
-"""
-
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import cv2
 import os
-import numpy as np
-
-# defining customized Dataset class for Udacity
-
+import sys
+import cv2
 import torch
-from torch.utils.data import Dataset, DataLoader, Sampler
-from torchvision import transforms, utils
-import random
-
-
+from torch.utils.data import Dataset
+import numpy as np
+sys.path.append('/home/norhan/SEA-RAFT/core')
+from raft import RAFT  # Importing the RAFT model
+from utils.utils import load_ckpt  # Utility to load model checkpoint
+from utils.flow_viz import flow_to_image  # Utility to visualize optical flow
+import pandas as pd
 
 class EvalDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None, optical_flow=True, public=False, private=False, img_size=(224, 224)):
+    def _init_(self, csv_file, root_dir, transform=None, optical_flow=True, public=False, private=False, img_size=(224, 224), raft_model_path=None, device='cuda', args=None):
         assert False in (public, private)
         mode = -1
         if public:
@@ -38,91 +24,98 @@ class EvalDataset(Dataset):
         camera_csv = pd.read_csv(csv_file)
 
         self.optical_flow = optical_flow
+        self.root_dir = root_dir
+        self.transform = transform
+        self.img_size = img_size
+        self.args = args
+
+        # Initialize SEA-RAFT model
+        self.device = torch.device(device)
+        self.raft_model = RAFT(self.args)  # Initialize RAFT model with arguments
+
+        if raft_model_path:
+            load_ckpt(self.raft_model, raft_model_path)  # Load pretrained weights
+
+        self.raft_model = self.raft_model.to(self.device)
+        self.raft_model.eval()  # Set RAFT model to evaluation mode
+
         csv_len = len(camera_csv)
         camera_csv['og_idx'] = range(len(camera_csv))
         self.camera_csv = camera_csv if mode == -1 else camera_csv[camera_csv['public'] == mode]
         self.original_camera = camera_csv
-        self.root_dir = root_dir
-        self.transform = transform
-        self.img_size = img_size
-    
-    def __len__(self):
-        return len(self.camera_csv)
-    
-    def read_data_single(self, idx):
-        #print(str(self.camera_csv['frame_id'].iloc[idx]) + '.jpg')
-        path = os.path.join(self.root_dir, str(self.camera_csv['frame_id'].iloc[idx]) + '.jpg')
-        #print(path)
-        image = cv2.imread(path)
-        
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = image[65:-25,:,:]
 
+    def _len_(self):
+        return len(self.camera_csv)
+
+    def compute_sea_raft_flow(self, img1, img2):
+        """
+        Computes optical flow using the SEA-RAFT model.
+        :param img1: The first image in the sequence (previous frame)
+        :param img2: The second image in the sequence (current frame)
+        :return: Optical flow between the two images
+        """
+        model_output = self.raft_model(img1, img2, iters=self.args.iters, test_mode=True)
+        flow = model_output['flow'][-1]  # Get the last flow prediction
+        return flow
+
+    def read_data_single(self, idx):
+        # Read and process the current image
+        path = os.path.join(self.root_dir, str(self.camera_csv['frame_id'].iloc[idx]) + '.jpg')
+        image = cv2.imread(path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = image[65:-25, :, :]  # Cropping the image as per original logic
+        original_img = image.copy()
+
+        # Get the steering angle and index from the CSV
         angle = self.camera_csv['steering_angle'].iloc[idx]
+        angle_t = torch.tensor(angle, dtype=torch.float32)  # Create tensor for the angle
+        idx_t = torch.tensor(idx, dtype=torch.long)  # Create tensor for the index
+
+        # Transform the image
         if self.transform:
             image_transformed = self.transform(cv2.resize(image, tuple(self.img_size)))
-        angle_t = torch.tensor(angle)
-        idx_t = torch.tensor(idx)
+
         if self.optical_flow:
+            # Handle optical flow calculation using SEA-RAFT
             og_idx = self.camera_csv['og_idx'].iloc[idx]
             if og_idx != 0:
-                path = os.path.join(self.root_dir, str(self.original_camera['frame_id'].iloc[og_idx - 1]) + '.jpg')
-                prev = cv2.imread(path)
+                prev_path = os.path.join(self.root_dir, str(self.original_camera['frame_id'].iloc[og_idx - 1]) + '.jpg')
+                prev = cv2.imread(prev_path)
                 prev = cv2.cvtColor(prev, cv2.COLOR_BGR2RGB)
-                prev = prev[65:-25,:,:]
-                prev = cv2.cvtColor(prev, cv2.COLOR_RGB2GRAY)
+                prev = prev[65:-25, :, :]  # Cropping the previous frame
             else:
-                prev = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-            cur = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-            # Use Hue, Saturation, Value colour model
-            flow = cv2.calcOpticalFlowFarneback(prev, cur, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-            hsv = np.zeros(image.shape, dtype=np.uint8)
-            hsv[..., 1] = 255
-            
-            mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-            hsv[..., 0] = ang * 180 / np.pi / 2
-            hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-            optical_rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-            optical_rgb = self.transform(cv2.resize(optical_rgb, tuple(self.img_size)))
-            del image
+                prev = original_img  # Use current image as previous if it's the first frame
+
+            # Prepare the images for SEA-RAFT model
+            prev_tensor = torch.tensor(prev, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0).to(self.device)
+            cur_tensor = torch.tensor(original_img, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0).to(self.device)
+
+            # Compute optical flow using SEA-RAFT
+            flow = self.compute_sea_raft_flow(prev_tensor, cur_tensor)
+
+            # Convert flow to a visual representation (RGB image)
+            optical_rgb = flow_to_image(flow[0].permute(1, 2, 0).cpu().numpy())
+
+            # Apply transformation to the optical flow data
+            if self.transform:
+                optical_rgb = self.transform(cv2.resize(optical_rgb, tuple(self.img_size)))
+
             return image_transformed, angle_t, idx_t, optical_rgb
-               
-        if self.transform:
-            del image
-            image = image_transformed
-        return image, angle_t, idx_t
-    
-    def read_data(self, idx):
-        if isinstance(idx, list):
-            data = None
-            for i in idx:
-                new_data = self.read_data(i)
-                if data is None:
-                    data = [[] for _ in range(len(new_data))]
-                for i, d in enumerate(new_data):
-                    data[i].append(new_data[i])
-                del new_data
-            indeces = [0, 1, 2] if not self.optical_flow else [0, 1, 2, 3]
-            for stack_idx in indeces: # we don't stack timestamp and frame_id since those are string data
-                data[stack_idx] = torch.stack(data[stack_idx])
-            
-            return data
-        
-        else:
-            return self.read_data_single(idx)
-    
-    def __getitem__(self, idx):
+
+        return image_transformed, angle_t, idx_t
+
+    def _getitem_(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        
-        data = self.read_data(idx)
-        
-        sample = {'image': data[0],
-                  'angle': data[1],
-                    'idx':data[2]}
+
+        data = self.read_data_single(idx)
+        sample = {
+            'image': data[0],
+            'angle': data[1],
+            'idx': data[2],
+        }
+
         if self.optical_flow:
             sample['optical'] = data[3]
-        del data
-        
-        return sample
 
+        return sample
